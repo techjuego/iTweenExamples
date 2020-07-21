943660bd6b8ba2974652c6e1d913bc7bbe032f57
techjuego <techjuego@gmail.com>
2020-07-22 01:41:25 +0530

    iTween.DrawPath,iTween.PutOnPath

---
+++ Assets/Examples/Draw Path & Put OnPath/Path Example1/PathExample.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathExample : MonoBehaviour
+{
+    public GameObject m_PlayerObj;
+    public Transform[] positionPoint;
+    [Range(0,1)]
+    public float value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(iTween.PathLength(positionPoint));
+    }
+    float tempTime;
+    // Update is called once per frame
+    void Update()
+    {
+        if (value < 1)
+        {
+            value += Time.deltaTime / 10;
+        }
+        iTween.PutOnPath(m_PlayerObj, positionPoint, value);
+    }
+    private void OnDrawGizmos()
+    {
+        
+        iTween.DrawPath(positionPoint,Color.green);
+    }
+}
