b97d75394a0d533c6dc4d2bdb5de91a1defb4d4d
techjuego <techjuego@gmail.com>
2020-07-21 23:34:24 +0530

    Ignoretimescale,Pasue,Resume

---
+++ Assets/Examples/Ignoretimescale/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject m_PauseUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_PauseUI.transform.localPosition = new Vector3(720, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+    }
+    public void PauseButton()
+    {
+        Time.timeScale = 0;
+        iTween.MoveTo(m_PauseUI,iTween.Hash("x",0,"time",1,"ignoretimescale",true,"islocal",true));
+    }
+    public void ResumeButton()
+    {
+        Time.timeScale = 1;
+        iTween.MoveTo(m_PauseUI, iTween.Hash("x", 720, "time", 1, "ignoretimescale", true, "islocal", true));
+    }
+    //
+   
+}
---
+++ Assets/Examples/Pasue Resume/PauseAndResumeExample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseAndResumeExample : MonoBehaviour
+{
+    public GameObject[] box;
+
+    public void PlayTween()
+    {
+        for (int i = 0; i < box.Length; i++)
+        {
+            Vector3 newpos = box[i].transform.position + new Vector3(20, 0, 0);
+            iTween.MoveTo(box[i],iTween.Hash("position",newpos,"time",Random.Range(5,10)));
+        }
+    }
+    public void PauseTween()
+    {
+        iTween.Pause();
+    }
+    public void ResumeTween()
+    {
+        iTween.Resume();
+    }
+}
